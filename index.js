a9a7f90d433a037ad8ac1b06130194f46072c084
unknown <dstapletongo@gmail.com>
2021-02-05 22:20:58 -0500

    inital setup
    for both readme and html

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
+const fs = require("fs");
+const generateReadme= require("./src/readme-template");//readme template
+const generateHtml = require("./src/page-template.js");// html template
 // store user input
 const profileData = process.argv.slice(2,process.argv.length);
+const [file_type,name_,github_] = profileData;
 
-const printProfileData = profileDataArray => {
-    profileDataArray.forEach(element => console.log(element));
-}
+const createDocument = (filetype, username, githublink) =>{
+    if (filetype === "readme") {
+        fs.writeFile("./test/index.md", generateReadme(username,githublink), err=>{
+            if (err) throw new Error(err);
+        });
+        console.log("Readme complete!");
+    }
+    else if (filetype === "profile") {
+        fs.writeFile("./test/index.html", generateHtml(username,githublink), err=>{
+            if (err) throw new Error(err);
+        });
+        console.log("Portfolio complete!");
+    }
+};
 
-// print user input
-printProfileData(profileData);
+//
+createDocument(file_type, name_, github_);
---
+++ test/index.md
@@ -0,0 +1,4 @@
+
+# David
+[Github link](https://github.com/dStapleton)
+    
